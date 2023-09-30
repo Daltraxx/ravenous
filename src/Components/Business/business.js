9d6f577072e4cbbdc3ac7ae08ea00578e9c4c9ec
Dalton Pettus <daltpettus@gmail.com>
2023-09-30 15:02:22 -0700

    create initial  shell of business component

--- src/Components/Business/business.js
+++ src/Components/Business/business.js
@@ -1,1 +1,24 @@
 import React from 'react';
+//Each Business component should display an image, name, address, city, state, zipcode, category, rating, and review count.
+
+function BusinessCompononent {
+    return (
+        <div>
+            <div /*holds business image*/>
+                <img />
+            </div>
+            <h2 /*holds business name*/></h2>
+            <div /*holds business address*/>
+                <p>{/*address*/}</p>
+                <p>{/*city*/}</p>
+                <p>{/*state and zipcode*/}</p>
+            </div>
+            <div /*other information*/>
+                <p>{/*category*/}</p>
+                <p>{/*rating*/}</p>
+                <p>{/*review count*/}</p>
+            </div>
+        </div>
+    )
+};
+
