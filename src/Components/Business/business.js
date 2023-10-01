dc2684249d020e2a7d2c3cd14ff6d54a0251a3fe
Dalton Pettus <daltpettus@gmail.com>
2023-10-01 09:22:55 -0700

    add hardcoded test business and image file

--- src/Components/Business/business.js
+++ src/Components/Business/business.js
@@ -1,7 +1,20 @@
 import React from 'react';
 //Each Business component should display an image, name, address, city, state, zipcode, category, rating, and review count.
+//create a hardcoded business that can be used to generate a list of fake businesses and simulate the website’s functionality.
 
-function BusinessCompononent {
+const testBusiness = {
+    businessImage : "KrustyKrab.png",
+    businessName : "Krusty Krab",
+    businessAddress : "831 Bottom Feeder Lane",
+    businessCity : "Bikini Bottom",
+    businessState : "Pacific Ocean",
+    businessZipCode: "00000",
+    businessCategory: "Fast Food",
+    businessRating: 4.9,
+    businessReviewCount: 1000000
+};
+
+function BusinessCompononent() {
     return (
         <div>
             <div /*holds business image*/>
