84bca77f396e042c369cc221eb0e4f8dbad2e02c
Dalton Pettus <daltpettus@gmail.com>
2023-10-02 19:10:54 -0700

    begin creating searchbar component, likely needs redesign using forms, research required

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+/*The search bar should contain two inputs, one for the user’s search terms and the selected location. Don’t forget a search button as well. 
+The search bar should also offer sorting options such as Best Match, Highest Rated, and Most Reviewed. 
+This functionality will be fully incorporated in a later part of the project, but for now, concentrate on building the buttons and the structure needed to communicate with the Yelp API.*/
+
+const sortOptions = {
+    "Best Match" : "best_match",
+    "Highest Rated" : "rating",
+    "Most Reviewed" : "review_count"
+};
+
+function SearchBar() {
+    function renderSortOptions() {
+        return Object.keys(sortOptions).map((sortOption) => {
+            let sortOptionValue = sortOptions[sortOption];
+            return <li key={sortOptionValue}>{sortOption}</li>;
+        });
+    };
+
+    // consider using forms to redesign below, such as a submit input
+    return (
+        <div /*search bar container*/>
+            <div /*search bar sort options*/>
+                <ul>{renderSortOptions()}</ul>
+            </div>
+            <div /*the two inputs*/>
+                <input placeholder="search terms" />
+                <input placeholder="location" />
+            </div>
+            <div /*submit button*/>
+                <input type="submit" />
+            </div>
+        </div>
+    )
+};
+
+export default SearchBar;
