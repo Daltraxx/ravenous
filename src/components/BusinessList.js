0866ca07744931025b45b1a7f05013562a87094d
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-04-10 11:28:47 -0700

    Successfully render BusinessList in App with mock data

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -4,21 +4,20 @@ import mockBusinessArray from '../mockData/mockBusinessData';
 
 function BusinessList() {
     return (
-        {mockBusinessArray.map((mockBusiness) => (
+        mockBusinessArray.map(mockBusiness => (
             <Business 
             src={mockBusiness.image} 
             alt={mockBusiness.alt} 
             businessName={mockBusiness.name}
             businessAddress={mockBusiness.address}
-            businessAddress={mockBusiness.city}
+            businessCity={mockBusiness.city}
             businessState={mockBusiness.state}
             businessZipcode={mockBusiness.zipcode}
             businessCategory={mockBusiness.category}
             businessRating={mockBusiness.rating}
             businessReviewCount={mockBusiness.reviewCount}
             />
-        ))}
-    );
+        ))
+    )
 }
-
 export default BusinessList;
