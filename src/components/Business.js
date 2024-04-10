193bac7cdd3299a16452b47c8a144ab8fccab21f
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-04-10 10:18:25 -0700

    Create Framework for Business Component

---
+++ src/components/Business.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Business() {
+    return (
+        <section>
+            <img />
+            <h2>Business Name</h1>
+            <p>Address</p>
+            <p>City</p>
+            <p>State and Zipcode</p>
+            <h3>Category</h3>
+            <p>Rating</p>
+            <p>Review Count</p>
+        </section>
+    )
+}
+
+export default Business;
