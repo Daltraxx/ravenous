c03e0fa704ebcb7b8e9173da195d3edcc4608c15
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-04-10 10:54:01 -0700

    Start completing BusinessList Component  and export it
    props are using data from mockBusinessArray

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -3,5 +3,22 @@ import Business from './Business';
 import mockBusinessArray from '../mockData/mockBusinessData';
 
 function BusinessList() {
-    
+    return (
+        {mockBusinessArray.map((mockBusiness) => (
+            <Business 
+            src={mockBusiness.image} 
+            alt={mockBusiness.alt} 
+            businessName={mockBusiness.name}
+            businessAddress={mockBusiness.address}
+            businessAddress={mockBusiness.city}
+            businessState={mockBusiness.state}
+            businessZipcode={mockBusiness.zipcode}
+            businessCategory={mockBusiness.category}
+            businessRating={mockBusiness.rating}
+            businessReviewCount={mockBusiness.reviewCount}
+            />
+        ))}
+    );
 }
+
+export default BusinessList;
