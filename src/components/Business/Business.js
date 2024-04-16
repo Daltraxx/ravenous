d90db1874d4823fff2afff6670bcde1277892b56
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-04-16 10:08:49 -0700

    Style business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import styles from './Business.module.css';
 
 function Business(props) {
     return (
-        <section>
+        <section className={styles.BusinessContainer}>
             <img src={props.src} alt={props.alt}/>
             <h2>{props.businessName}</h2>
-            <div>
-                <p>{props.businessAddress}</p>
-                <p>{props.businessCity}</p>
-                <p>{props.businessState} {props.businessZipcode}</p>
-            </div>
-            <div>
-                <h3>{props.businessCategory}</h3>
-                <p>{props.businessRating} stars</p>
-                <p>{props.businessReviewCount} reviews</p>
+            <div className={styles.BusinessDetailsContainer}>
+                <div className={styles.BusinessAddressContainer}>
+                    <p>{props.businessAddress}</p>
+                    <p>{props.businessCity}</p>
+                    <p>{props.businessState} {props.businessZipcode}</p>
+                </div>
+                <div className={styles.BusinessRatingContainer}>
+                    <h3>{props.businessCategory}</h3>
+                    <p>{props.businessRating} stars</p>
+                    <p>{props.businessReviewCount} reviews</p>
+                </div>
             </div>
         </section>
     )
