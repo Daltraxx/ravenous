76be736e7e14a40b761d97c8488fbd18467e2322
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-04-27 09:51:57 -0700

    Make Footer a component and remove it from index.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from './App.module.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Footer from '../Footer/Footer';
 import Yelp from '../../utils/Yelp';
 
 function App() {
@@ -13,20 +14,25 @@ function App() {
     setBusinesses((prev) => businesses);
     console.log(businesses);
   }
+  
+
+  //const [displayResults, setDisplayResults] = useState(false);
 
   return (
     <div className={styles.App}>
       <header>
         <h1>Ravenous</h1>
         <nav>
-          <SearchBar searchYelp={searchYelp}/>
+          <SearchBar searchYelp={searchYelp} />
         </nav>
       </header>
       <main>
         <BusinessList businesses={businesses} />
       </main>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.Footer}>
+            <h4>&copy; Daltraxx Inc. 2024</h4>
+        </footer>
+    );
+}
+
+export default Footer;
